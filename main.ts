dd882a2e0598280211ad92686446909b25b3e89d
Ed <ea.torresl1@uniandes.edu.co>
2023-10-16 15:59:07 -0500

    Update: Uso de comillas

--- main.ts
+++ main.ts
@@ -3,3 +3,8 @@ console.log("Hola Mundo");
 // Crear variables
 let miTexto: string="Hola mundo de nuevo";
 console.log(miTexto);
+
+let tagUno: string="<div class>=\"ejemplo\">";
+let tagDos: string="<div class>='ejemplo'>";
+console.log(tagUno);
+console.log(tagDos);
--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,7 @@ console.log("Hola Mundo");
 // Crear variables
 var miTexto = "Hola mundo de nuevo";
 console.log(miTexto);
+var tagUno = "<div class>=\"ejemplo\">";
+var tagDos = "<div class>='ejemplo'>";
+console.log(tagUno);
+console.log(tagDos);
