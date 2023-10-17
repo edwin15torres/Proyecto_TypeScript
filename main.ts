fde3114dc8c0e71c80c1bb9528c3879679c07b5b
Ed <ea.torresl1@uniandes.edu.co>
2023-10-16 21:51:30 -0500

    instancia aprendiz

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { Aprendiz, NivelEducativo } from './aprendiz.js';
+
+export const ap = new Aprendiz("Juan Pablo", 
+                                "Reyes Gómez",
+                                "avatar.png",
+                                30,
+                                NivelEducativo.POSTGRADO);
+
+console.log(ap);                                
